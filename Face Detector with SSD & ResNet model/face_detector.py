34a3b163e5aef2f80fb491b38577878ce46f31a2
Random Nerd <32665974+clair513@users.noreply.github.com>
2019-02-27 16:08:53 +0530

    Aesthetical changes

--- Face Detector with SSD & ResNet model/face_detector.py
+++ Face Detector with SSD & ResNet model/face_detector.py
@@ -9,7 +9,7 @@ parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-i', '--image', required=True)
 parser.add_argument('-m', '--model', required=True)
 parser.add_argument('-p', '--prototxt', required=True)
-parser.add_argument('-c', '--confidence_interval', type=float, default=0.5)
+parser.add_argument('-c', '--conf', type=float, default=0.5)
 # Creating Python dictionary from parsed arguments:
 args = vars(parser.parse_args())
 
@@ -22,16 +22,16 @@ img = cv2.imread(args['image'])
 # Pre-processing image [Mean subtraction(To control illumination changes by computing avg pixel intensity for RGB & subtracting that fro Input image) and manual Scaling (for Normalization)] to 4-Dimensional image:
 img_blob = cv2.dnn.blobFromImage(cv2.resize(img, (300,300)), 1.0, (300,300), (104.0,177.0,123.0))
 
-# Feed image blob into model for detection:
+# Feed image blob into model for face detection:
 network.setInput(img_blob)
 face= network.forward()
 
 # Loop over detections to filter weak detections based on minimum C.I, then add bounding box:
 for i in range(0, face.shape[2]):
     ci= face[0,0,i,2]
-    if ci>args['confidence_interval']:
-        box = face[0,0,i,3:7] * np.array([w,h,w,h])
-        (tl,tr,bl,br) = box.astype('int')
+    if ci>args['conf']:
+        bbox = face[0,0,i,3:7] * np.array([w,h,w,h])
+        (tl,tr,bl,br) = bbox.astype('int')
         text = '{:.2f}%'.format(ci*100)
 
         if tr - 10 > 10:
@@ -40,10 +40,9 @@ for i in range(0, face.shape[2]):
             y = tr + 10
 
         cv2.rectangle(img, (tl,tr), (bl,br), (0,128,0), 2)
-        cv2.putText(img, text, (tl,y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (252,21,0), 2)
+        cv2.putText(img, text, (tl,y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,0,0), 2)
 
 # Image Output:
-print(cv2.__version__)
 cv2.imshow('Output Image',img)
 cv2.waitKey(0)
 
