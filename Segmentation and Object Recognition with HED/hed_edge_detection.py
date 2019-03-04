5cbc8c20d34acfc66149a4a9dc673daaef235968
Random Nerd <32665974+clair513@users.noreply.github.com>
2019-03-05 05:14:59 +0530

    Added HED Edge Detection codesheet

---
+++ Segmentation and Object Recognition with HED/hed_edge_detection.py
@@ -0,0 +1,43 @@
+# Loading required libraries:
+import cv2
+
+# PATH to pre-trained model files:
+protoFile = '/home/alok/Desktop/hed/model/deploy.prototxt'
+weightsFile = '/home/alok/Desktop/hed/model/hed_pretrained.caffemodel'
+
+img = cv2.imread('/home/alok/Desktop/hed/images/Input_cr7.jpg')
+H, W = img.shape[:2]
+
+class CropLayer(object):
+    def __init__(self, params, blobs):
+        self.x_start = 0
+        self.x_end = 0
+        self.y_start = 0
+        self.y_end = 0
+
+    def getMemoryShapes(self, inputs):
+        input_shape, target_shape = inputs[0], inputs[1]
+        batch_size, num_channels = input_shape[0], input_shape[1]
+        height, width = target_shape[2], target_shape[3]
+
+        self.y_start = (input_shape[2] - target_shape[2]) // 2
+        self.x_start = (input_shape[3] - target_shape[3]) // 2
+        self.y_end = self.y_start + height
+        self.x_end = self.x_start + width
+
+        return [[batch_size, num_channels, height, width]]
+
+    def forward(self, inputs):
+        return [inputs[0][:, :, self.y_start:self.y_end, self.x_start:self.x_end]]
+
+
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+img_blob = cv2.dnn.blobFromImage(img, scalefactor=1.0, size=(W, H), mean=(104.00698793, 116.66876762, 122.67891434), swapRB=False, crop=False)
+net.setInput(img_blob)
+out = net.forward()
+out = cv2.resize(out[0, 0], (W, H))
+out = (255 * out).astype('uint8')
+
+cv2.imwrite('/home/alok/Desktop/hed/images/Output_hedImage_cr7.jpg', out)
+cv2.imshow('HED Output', out)
+cv2.waitKey(0)
